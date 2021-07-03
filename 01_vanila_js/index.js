dcf5e08dfd12217de3215d62ab4f08c258589664
guna9 <osh01039@naver.com>
2021-07-03 14:10:07 +0900

    sexy nicolas vanilla js 2-7

--- 01_vanila_js/index.js
+++ 01_vanila_js/index.js
@@ -1,23 +1,24 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = 'clicked';
 
 function handleClick(){
-    title.innerHTML = "change font color !"
-    const currentColor = title.style.color;
-    console.log(currentColor);
-    if (currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
+    title.classList.toggle(CLICKED_CLASS);
+    /*const hasClass = title.classList.contains(CLICKED_CLASS);
+    if(hasClass){
+        title.classList.remove(CLICKED_CLASS);
+        console.log("remove 'clicked' class");
     }else {
-        title.style.color = BASE_COLOR;
-    }
+        title.classList.add(CLICKED_CLASS);
+        console.log("add 'clicked' class");
+    }*/
+    //위에랑 같은 내용이지만 토글로 간략하게 줄임
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
+
 init();
 
-
+//https://developer.mozilla.org/ko/docs/Web/API/Element/classList classList에 사용가능한 메서드 참조
